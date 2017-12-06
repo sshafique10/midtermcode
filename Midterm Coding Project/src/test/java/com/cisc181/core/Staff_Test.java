d038d9df731cfeced33c793d1aa5b2664eb25e4e
saad <saad@saad-PC.fios-router.home>
2017-12-06 15:20:43 -0500

    Final

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -1,22 +1,67 @@
 package com.cisc181.core;
 
 import static org.junit.Assert.*;
+import java.util.Date;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Staff_Test {
+import com.cisc181.eNums.eTitle;
 
+public class Staff_Test {
+	
+	
+	static ArrayList<Staff> staffers = new ArrayList<Staff>();
 	@BeforeClass
-	public static void setup() {
+	public static void setup() throws PersonException {
+		Staff st = new Staff(eTitle.MR);
+		st.setSalary(10000);
+		Staff st2 = new Staff(eTitle.MR);
+		st2.setSalary(20000);
+		Staff st3 = new Staff(eTitle.MR);
+		st3.setSalary(30000);
+		Staff st4 = new Staff(eTitle.MR);
+		st4.setSalary(40000);
+		Staff st5 = new Staff(eTitle.MR);
+		st5.setSalary(50000);
+		staffers.add(st);
+		staffers.add(st2);
+		staffers.add(st3);
+		staffers.add(st4);
+		staffers.add(st5);
+		
+		
+		
 	}
 	
 	@Test
-	public void test() {
-		assertEquals(1,1);
+	public void test() throws PersonException {
+		double avg = 0;
+		for(Staff st: staffers) {
+			avg += st.getSalary()/5;
+		}
+		
+		Assert.assertEquals(30000.0, avg, .01);
 	}	
+	
+	@Test
+	public void test2() throws PersonException {
+		try {
+			Date d = java.sql.Date.valueOf("1999-11-19");
+			Date d2 = java.sql.Date.valueOf("1990-07-15");
+			Staff st = new Staff("p", "j", "Dan", d, "153 GL", "111-222-333", "fake@gmail.com", "Offhour", 2, 20000, d2, eTitle.MR);
+		}
+		catch (PersonException e) {
+			Assert.assertEquals(1.0, 1.0, 0.01);
+			//Test passes if Person Exception is throw, if other exception is thrown assert will fail
+		}
+		catch (Exception e) {
+			Assert.assertEquals(1.0, 0.01, 0.01);
+		}
+	}
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -2,20 +2,58 @@ package com.cisc181.core;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.cisc181.eNums.eMajor;
+
 public class Student_Test {
-
+		//create empty array lists
+		static ArrayList<Course> Courses = new ArrayList<Course>();
+		static ArrayList<Semester> Semesters = new ArrayList<Semester>();
+		static ArrayList<Section> Sections = new ArrayList<Section>();
+		static ArrayList<Student> Students = new ArrayList<Student>();
+		static ArrayList<Enrollment> ClassMembers = new ArrayList<Enrollment>();
+	
 	@BeforeClass
-	public static void setup() {
-	}
-
-	@Test
-	public void test() {
-		assertEquals(1, 1);
-	}
+	public static void setUp() throws Exception{
+				
+		//add courses
+		Course CISC181 = new Course(UUID.randomUUID(), null, 0, null);
+		Courses.add(CISC181);
+		Course CHEM100 = new Course(UUID.randomUUID(), null, 0, null);
+		Courses.add(CHEM100);
+		Course MATH300 = new Course(UUID.randomUUID(), null, 0, null);
+		Courses.add(MATH300);
+		
+		//add semesters
+		Semester FallS = new Semester(UUID.randomUUID());
+		Semesters.add(FallS);
+		Semester SpringS = new Semester(UUID.randomUUID());
+		Semesters.add(SpringS);
+		
+		//add sections
+		for(Course c : Courses) {
+			for(Semester s : Semesters) {
+				Sections.add(new Section(c.getCourseID(),s.getSemesterID()));
+			}
+		}
+		
+		//add students
+		
+		
+		Students.add(new Student("Joe","A","D",eMajor.CHEM, "0","000-000-0000","jd@mail"));
+		Students.add(new Student("Bob","B","D",eMajor.COMPSI, "1","111-000-0000","bd@mail"));
+		Students.add(new Student("John","C","D",eMajor.NURSING, "2","222-000-0000","jd@mail"));
+		Students.add(new Student("Mike","D","D",eMajor.CHEM, "3","333-000-0000","md@mmail"));
+		Students.add(new Student("Bo","E","D",eMajor.BUSINESS, "4","444-000-0000","bd1@mail"));
+		Students.add(new Student("Ray","F","D",eMajor.BUSINESS, "5","555-000-0000","rd@mail"));
+		Students.add(new Student("Matt","G","D",eMajor.COMPSI, "6","666-000-0000","md2@mail"));
+		Students.add(new Student("Alex","H","D",eMajor.NURSING, "7","777-000-0000","ad@mail"));
+		Students.add(new Student("Jess","I","D",eMajor.CHEM, "8","888-000-0000","jd@mail"));
+		Students.add(new Student("Lindsay","J","D",eMajor.BUSINESS, "9","999-000-0000","ld@mail"));
+		
+		
 }
